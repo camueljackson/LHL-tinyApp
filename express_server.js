c41218d8ed456b81a9ec328c065908d605fb6f7b
camueljackson <camille.a.pelletier@gmail.com>
2018-06-13 03:51:39 +0000

    able to add short and long url to urls_index

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var app     = express();
-var PORT    = 8080;
+var express       =   require('express');
+var app           =   express();
+const bodyParser  =   require("body-parser");
+var PORT          =   8080;
 
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 
@@ -12,29 +14,65 @@ var urlDatabase = {
 };
 
 
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+}
+
+
+// GREETING
+app.get('/hello', (req, res) => {
+  res.end('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+
+
 // HOME
 app.get('/', (req, res) => {
   res.end('Hello!');
 });
 
 
-
-// URLS
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
-// SHOW URLS
+// DATABASE URLS
 app.get('/urls', (req, res) => {
-  res.render('urls_index');
+  let templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
 });
 
 
-// HELLO
-app.get('/hello', (req, res) => {
-  res.end('<html><body>Hello <b>World</b></body></html>\n');
+// NEW URLS
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
+
+// POST URLS
+app.post("/urls", (req, res) => {
+let shortURL = generateRandomString();
+let longURL = req.body.longURL
+urlDatabase[shortURL] = longURL
+res.redirect('/urls')
+});
+
+
+// UNIQUE URL
+app.get('/urls/:id', (req, res) => {
+  let shortURL = req.params.id;
+  let longURL = urlDatabase[shortURL];
+  let templateVars = {
+    shortURL: shortURL,
+    longURL: longURL
+  }
+
+  res.render('urls_show', templateVars);
+});
+
+
+// REDIRECT TO LONG URL
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+  res.send('OK');
+});
 
 
 // ***********************************************************************
@@ -42,3 +80,4 @@ app.get('/hello', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
